fd426a834d26c26b73504790b1a287f19bddc4d7
jsasser4 <jsasser4@uncc.edu>
2022-11-18 14:54:12 -0500

    Fix refactor bug

--- src/model/task.py
+++ src/model/task.py
@@ -14,8 +14,8 @@ class Task(db.Model):
     assigned_user_id: int = db.Column(Integer, ForeignKey("user.id"), nullable=True)
     assigned_user: Optional[User] = db.relationship("user", foreign_keys=[assigned_user_id])
 
-    sprint_id: int = db.Column(Integer, ForeignKey("sprint.html.id"))
-    sprint: Sprint = db.relationship("sprint.html", back_populates="tasks")
+    sprint_id: int = db.Column(Integer, ForeignKey("sprint.id"))
+    sprint: Sprint = db.relationship("sprint", back_populates="tasks")
 
     is_completed: bool = db.Column(Boolean())
     created_at: datetime = db.Column(DateTime())
