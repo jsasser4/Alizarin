21e5c1be2be9aecce0aa44933efc3e2d21171293
jsasser4 <jsasser4@uncc.edu>
2022-12-10 20:53:58 -0500

    Start code cleanup

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from datetime import datetime
-
 from flask import Flask, url_for, request, session, redirect
 from flask import render_template
 from bcrypt import hashpw, gensalt, checkpw
-from src.form import RegisterForm, LoginForm, ProjectForm, SprintForm, TaskForm
 
+from src.form import RegisterForm, LoginForm, ProjectForm, SprintForm, TaskForm
 from src import db
-from src.model.task import Task
 from src.model.user import User
-from src.model.story import Story
-from src.model.sprint import Sprint
-from src.model.project import Project
-from src.model.login_attempt import LoginAttempt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
@@ -25,114 +19,10 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/test')
-def test():
-    return render_template('app/test.html')
-
-
 @app.route('/')
 @app.route('/index')
 def index():
     return redirect(url_for('login'))
-
-
-"""
-@app.route('/projects/<project_id>/<sprint_id>', methods=['POST', 'GET'])
-def add_task(project_id, sprint_id):
-    current_user = db.session.query(User).filter_by(id=session['user_id']).first_or_404()
-    current_sprint = db.session.query(Sprint).filter_by(id=sprint_id, project_id=project_id)
-    form = TaskForm()
-    if request.method == "POST" and form.validate_on_submit():
-        name = request.form['name']
-        desc = request.form['desc']
-        sprint = Task(name=name, description=desc, sprint=current_sprint)
-        db.session.add(sprint)
-        db.session.commit()
-    return render_template('app/sprints.html', user=current_user, form=form)
-"""
-
-
-@app.route('/project/add', methods=['GET', 'POST'])
-def add_project():
-    current_user = db.session.query(User).filter_by(id=session['user_id']).one()
-    form = ProjectForm()
-    if request.method == "POST" and form.validate_on_submit():
-        name = request.form['name']
-        comment = request.form['comment']
-        project = Project(name=name, comment=comment, created_by=current_user)
-        db.session.add(project)
-        db.session.commit()
-        return redirect(url_for('get_projects'))
-    return render_template('app/add.html', form=form)
-
-
-@app.route('/sprint', methods=['GET', 'POST'])
-def add_sprint():
-    current_user = db.session.query(User).filter_by(id=session['user_id']).one()
-    form = SprintForm()
-    if request.method == "POST" and form.validate_on_submit():
-        name = request.form['name']
-        sprint = Sprint(name=name,  created_by=current_user)
-        db.session.add(sprint)
-        db.session.commit()
-        return redirect(url_for('get_sprints'))
-    return render_template('app/addSprint.html', form=form)
-
-@app.route('/project/edit/<project_id>', methods=['GET', 'POST'])
-def edit_project(project_id):
-    project = db.session.query(Project).filter_by(id=project_id).one()
-    if project is None:
-        return redirect(url_for('get_projects'))
-    form = ProjectForm()
-    form.name.data = project.name
-    form.comment.data = project.comment
-
-    if request.method == "POST" and form.validate_on_submit():
-        project_name = request.form['name']
-        comment = request.form['comment']
-        project.name = project_name
-        project.comment = comment
-        db.session.commit()
-        return redirect(url_for('get_projects'))
-
-    return render_template('app/edit.html', form=form, project=project)
-
-@app.route('/project/delete/<project_id>', methods=['POST'])
-def delete_project(project_id):
-    current_user = db.session.query(User).filter_by(id=session['user_id']).one()
-    project = db.session.query(Project).filter_by(id=project_id).one()
-    db.session.delete(project)
-    db.session.commit()
-    return render_template('app/projects.html')
-
-
-@app.route('/project/<project_id>', methods=['GET'])
-def view_project(project_id):
-    current_user = db.session.query(User).filter_by(id=session['user_id']).one()
-    current_project = db.session.query(Project).filter_by(id=project_id).first_or_404()
-    return render_template('app/project.html', project=current_project)
-
-
-@app.route('/projects', methods=['GET'])
-def get_projects():
-    current_user = db.session.query(User).filter_by(id=session['user_id']).one()
-    user_projects = db.session.query(Project).filter_by(created_by=current_user).all()
-    if len(user_projects) == 0:
-        return redirect(url_for('add_project'))
-    return render_template('app/projects.html', projects=user_projects)
-
-@app.route('/home')
-def view_home():
-    return render_template('app/home.html')
-
-@app.route('/sprints/<project_id>', methods=['GET'])
-def get_sprints(project_id):
-    pid = project_id
-    current_user = db.session.query(User).filter_by(id=session['user_id']).one()
-    project_sprints = db.session.query(Sprint).filter_by(project_id=pid).all()
-    if len(project_sprints) == 0:
-        return redirect(url_for('add_sprint'))
-    return render_template('app/sprints.html', sprints=project_sprints)
 
 
 @app.route('/register', methods=['POST', 'GET'])
