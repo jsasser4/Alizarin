d68e0fa5db6f25d81384517b7c05da0ff4faaaad
Lindsey Varner <lvarner1@uncc.edu>
2022-12-15 03:41:22 -0500

    Style changes

--- src/form.py
+++ src/form.py
@@ -9,19 +9,19 @@ from .model.user import User
 class RegisterForm(FlaskForm):
     class Meta:
         csrf = False
-    firstname = StringField('First Name', validators=[Length(1, 10)])
-    lastname = StringField('Last Name', validators=[Length(1, 20)])
+    firstname = StringField('First name', validators=[Length(1, 10)])
+    lastname = StringField('Last name', validators=[Length(1, 20)])
 
     email = StringField('Email', [
-        Email(message='Not a valid email address.'),
+        Email(message='Invalid email address'),
         DataRequired()])
 
     password = PasswordField('Password', [
-        DataRequired(message="Please enter a password."),
+        DataRequired(message="Please enter a password"),
         EqualTo('confirmPassword', message='Passwords must match')
     ])
 
-    confirmPassword = PasswordField('Confirm Password', validators=[
+    confirmPassword = PasswordField('Confirm password', validators=[
         Length(min=6, max=10)
     ])
 
@@ -34,50 +34,50 @@ class LoginForm(FlaskForm):
         csrf = False
 
     email = StringField('Email', [
-        Email(message='Not a valid email address.'),
+        Email(message='Invalid email address'),
         DataRequired()])
     password = PasswordField('Password', [
-        DataRequired(message="Please enter a password.")])
+        DataRequired(message="Please enter a password")])
 
     submit = SubmitField('Submit')
 
     # noinspection PyMethodMayBeStatic
     def validate_email(self, field):
         if db.session.query(User).filter_by(email=field.data).count() == 0:
-            raise ValidationError('Incorrect username or password.')
+            raise ValidationError('Incorrect username or password')
 
 
 class ProjectForm(FlaskForm):
     class Meta:
         csrf = False
-    name = StringField('Project Name', validators=[Length(1, 32)])
-    submit = SubmitField('Create Project')
+    name = StringField('Project name', validators=[Length(1, 32)])
+    submit = SubmitField('Create project')
 
 
 class SprintForm(FlaskForm):
     class Meta:
         csrf = False
-    name = StringField('Sprint Name', validators=[Length(1, 32)])
-    submit = SubmitField('Create Sprint')
+    name = StringField('Sprint name', validators=[Length(1, 32)])
+    submit = SubmitField('Create sprint')
 
 
 class TaskForm(FlaskForm):
     class Meta:
         csrf = False
-    name = StringField('Task Name', validators=[Length(1, 32)])
-    description = TextAreaField('Sprint Description', validators=[Length(1, 1024)])
-    submit = SubmitField('Add task to Sprint')
+    name = StringField('Task name', validators=[Length(1, 32)])
+    description = TextAreaField('Sprint description', validators=[Length(1, 1024)])
+    submit = SubmitField('Add task to sprint')
 
 
 class UserForm(FlaskForm):
     class Meta:
         csrf = False
-    email = StringField('Email', [Email(message='Not a valid email address.'), DataRequired()])
-    submit = SubmitField('Add Project User')
+    email = StringField('Email', [Email(message='Invalid email address.'), DataRequired()])
+    submit = SubmitField('Add project user')
 
 
 class UserStory(FlaskForm):
     class Meta:
         csrf = False
-    content = TextAreaField('Story Content', validators=[Length(1, 1024)])
-    submit = SubmitField('Create Project Story')
+    content = TextAreaField('Story description', validators=[Length(1, 1024)])
+    submit = SubmitField('Create user story')
